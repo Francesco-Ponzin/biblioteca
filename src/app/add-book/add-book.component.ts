c18d8d8521b3a84034636cd639ae4f3c546f4dfe
Francesco <francesco.ponzin@gmail.com>
2020-05-03 08:03:32 +0200

    add validation

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -13,27 +13,78 @@ export class AddBookComponent implements OnInit {
 
   title: string = "aggiungi un libro";
   newBook: Book;
+  error = {
+    ISBN: false,
+    title: false,
+    author: false,
+    publisher: false,
+    publicationDate: false,
+    coverUrl: false
+  }
 
   ngOnInit(): void {
-   this.reset();
+    this.reset();
 
   }
 
-  reset():void{
+  reset(): void {
     this.newBook = {
       ISBN: "",
       title: "",
       author: "",
       publisher: "",
-      publicationDate: new Date(),
+      publicationDate: null,
       coverUrl: ""
     };
   }
 
 
   add() {
-    this.bookService.addBook(this.newBook);
-    this.reset();
+    let validate = true;
+    if (new RegExp("^[0-9]{13}$").test(this.newBook.ISBN)) {
+      this.error.ISBN = false;
+    } else {
+      this.error.ISBN = true;
+      validate = false;
+      this.newBook.ISBN = "";
+    }
+
+    if(this.newBook.title.length > 0){
+      this.error.title = false;
+    }else{
+      this.error.title = true;
+      validate = false;
+    }
+
+    if(this.newBook.author.length > 0){
+      this.error.author = false;
+    }else{
+      this.error.author = true;
+      validate = false;
+    }
+
+
+
+    if(this.newBook.publisher.length > 0){
+      this.error.publisher = false;
+    }else{
+      this.error.publisher = true;
+      validate = false;
+    }
+
+    if(this.newBook.publicationDate != null){
+      this.error.publicationDate = false;
+    }else{
+      this.error.publicationDate = true;
+      validate = false;
+    }
+
+    if (validate) {
+      this.bookService.addBook(this.newBook);
+      this.reset();
+    }
+
+
   }
 
 }
